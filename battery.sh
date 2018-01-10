91b195aa967e30af6e14d65b93e2db41b219de05
Jose Arenado <vidalstat@gmail.com>
2018-01-09 19:29:36 -0500

    tweaked battery script

--- battery.sh
+++ battery.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+
+CURRENT="high"
 while true
 do
+  if [[ $CURRENT -eq "high" ]]; then
     battery_level=`acpi -b |head -1| grep -P -o '[0-9]+(?=%)'`
-    if [ $battery_level -le 10 ]; then
-       # notify-send -t 0 -u critical "Battery is below 10%!" "Plug me in, asshole!"    
-       /usr/bin/espeak "Battery below 10 percent. Plug me in, asshole"
-       i3-nagbar -t warning -m "Battery is below 10%!" "Plug me in, asshole!"    
+    echo $CURRENT
+    if [ $battery_level -le 90 ]; then
+      # notify-send -t 0 -u critical "Battery is below 10%!" "Plug me in, asshole!"    
+      /usr/bin/espeak "Battery below 10 percent. Plug me in, asshole"
+      i3-nagbar -t warning -m "Battery is below 10%!" "Plug me in, asshole!"    
+      CURRENT="low"
     fi
+  else
 
-    sleep 100 # 300 seconds or 5 minutes
+    echo $CURRENT
+    continue
+  fi
+
+  sleep 300 # 300 seconds or 5 minutes
 done
